3a90a6dc38a26a9ce57001795c41038bd7f2fc9c
DylanEHolland <salinson1138@gmail.com>
2020-09-10 20:57:07 -0500

    Implemented agent initiation

--- datablox/agent.py
+++ datablox/agent.py
@@ -6,6 +6,7 @@ import json
 from .hoarder import hoarder
 
 class datablox_agent(object):
+    agent_list = None
     created_at = None
     db_directory = None
     filename = None
@@ -18,7 +19,6 @@ class datablox_agent(object):
     socket = None
 
     def __init__(self, *args, **kwargs):
-        
         if len(args):
             self.name = args[0]
 
@@ -34,6 +34,7 @@ class datablox_agent(object):
             self.db.directory = "./"
         self.directory = "%s/agents" % self.db_directory
         self.filename = "%s/%s" % (self.directory, self.name)
+        self.agent_list = []
         if os.path.isfile(self.filename):
             self.load()
         self.socket = subs.create_socket()
@@ -58,6 +59,9 @@ class datablox_agent(object):
                 subs.mkdir_recursively(self.directory)
             
             subs.write_file(self.filename, json.dumps(self.to_dict()))
+            self.agent_list.append(self.signature)
+        subs.write_file(self.db_directory + "/agents.json", json.dumps(self.agent_list))
+        self.committed = True
 
     def digest(self):
         buffer = "%s-%s-%s-%s-%s" % (
@@ -73,6 +77,11 @@ class datablox_agent(object):
     def load(self):
         buffer = subs.read_file(self.filename)
         buffer = json.loads(buffer)
+        if os.path.isfile("%s/agents.json" % self.db_directory):
+            agents_buffer = subs.read_file("%s/agents.json" % self.db_directory)
+            agents_buffer = json.loads(agents_buffer)
+            self.agent_list = agents_buffer
+
         self.name = buffer['name']
         self.created_at = subs.datetime_from_string(buffer['created_at'])
         self.public_address = buffer['public_address']
@@ -86,32 +95,67 @@ class datablox_agent(object):
 
     def serve(self):
         self.socket.bind((self.local_address, 11001))
-        self.socket.listen(1)
+        self.socket.listen(5)
 
         while True:
             try:
                 connection, addr = self.socket.accept()
+                print(addr)
                 incoming = self.receive(connection)
-                
-                if "test" in incoming:
-                    print("Replying to test")
-                    self.send("Test callback", connection=connection)
+                print(incoming, type(incoming))
+
+                if not incoming:
+                    self.send("[Error] You sent nothing", connection=connection)
                 else:
-                    print(incoming)                
-                    self.send("Something else", connection=connection)
+                    if type(incoming) == dict and "test" in incoming:
+                        print("Replying to test")
+                        self.send("Test callback", connection=connection)
+                    else:
+                        agent = None                        
+                        if type(incoming) == str:
+                            agent = incoming                    
+                            if agent not in self.agent_list:
+                                print("Discovered new agent")
+                                self.send(self.agent_list, connection = connection)
+                                self.agent_list.append(agent)
+                                self.commit()
+                            
+                                self.send("Success", connection = connection)
+                        elif type(incoming) == dict:
+                            if "agent" in incoming:
+                                agent = incoming.get("agent")
+
+                        if not agent:
+                            self.send("[Error] Agent required without test attr", connection=connection)
+
+                        if agent not in self.agent_list:
+                            self.send("[Error] Agent unknown. Initiate before making requests.", connection=connection)
+                        self.send("[Warning] nothing to do", connection=connection)
+
             except KeyboardInterrupt as err:
                 print("Cleanly shutting down")
                 self.socket = None
                 break
+
     def connect(self):
         self.socket.connect((self.local_address, 11001))
 
     def initiate(self):
         # Initiate session with server
-        if not self.socket:
-            self.connect()
+        if not self.committed:
+            print("[Error] Must commit agent before making calls to other nodes")
+            return False
 
-        self.send(self.signature)
+        self.connect()
+        server_connection = self.send(self.signature)
+        buffer = self.receive(server_connection)
+        if type(buffer) == list:
+            print(buffer)
+        else:
+            if "Error" in buffer:
+                print("Something went wrong:", buffer)
+                
+        
 
     def receive(self, connection):
         char = None
@@ -130,8 +174,7 @@ class datablox_agent(object):
         
         return buffer
 
-    def send(self, data, connection = None):        
-        data['agent'] = self.signature
+    def send(self, data, connection = None):
         data = json.dumps(data).encode('utf-8')
         
         if not connection:
